c061071fcfd7a03d865e789e32a57b06b8b1d111
iamtahirkhan6 <48375147+iamtahirkhan6@users.noreply.github.com>
2021-03-13 19:26:08 +0530

    Update dropdown.blade.php

--- resources/views/components/forms/basic-stacked/dropdown.blade.php
+++ resources/views/components/forms/basic-stacked/dropdown.blade.php
@@ -1,8 +1,7 @@
-@if(isset($array))
-    <select class="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" {{ $attributes }}>
+@props(['array'])
+<select class="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" {{ $attributes }}>
         <option hidden>{{ $attributes["title"] ?? 'Select an option' }}</option>
         @foreach($array as $key => $each)
             <option value="{{ $each->id ?? $key }}">{{ $each->{$attributes["arrayKey"]} ?? $each->name ?? $each->id ?? $each ?? '' }}</option>
         @endforeach
     </select>
-    @endif
